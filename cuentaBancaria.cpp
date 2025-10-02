9c117013f32fefb1f2627d7bfa074b7765f1317d
gabipicasso <picassogabriela6@gmail.com>
2025-10-02 19:17:25 -0300

    cuentabancaria

---
+++ cuentaBancaria.cpp
@@ -0,0 +1,3 @@
+#include "cuentaBancaria.h"
+#include <iostream>
+using namespace std;
---
+++ cuentaBancaria.h
@@ -0,0 +1,7 @@
+#ifndef FUNCIONES_H_INCLUDED
+#define FUNCIONES_H_INCLUDED
+#include <iostream>
+
+
+
+#endif // FUNCIONES_H_INCLUDED
