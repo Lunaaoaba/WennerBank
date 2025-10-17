b7e22dec0222904f21a7c29430234738ce35c7c5
Lunaaoaba <lunabaricalla1@gmail.com>
2025-10-16 23:45:32 -0300

    Update congif.h

--- congif.h
+++ congif.h
@@ -2,18 +2,18 @@
 #define CONFIG_H
 
 // NOMBRES DE ARCHIVOS
-const char* const NOMBRE_ARCHIVO_CLIENTES = "clientes.dat";
-const char* const NOMBRE_ARCHIVO_EMPLEADOS = "empleados.dat";
-const char* const NOMBRE_ARCHIVO_CUENTAS = "cuentas.dat";
-const char* const NOMBRE_ARCHIVO_PRESTAMOS = "prestamos.dat";
-const char* const NOMBRE_ARCHIVO_TRANSACCIONES = "transacciones.dat";
+const char* NOMBRE_ARCHIVO_CLIENTES = "clientes.dat";
+const char* NOMBRE_ARCHIVO_EMPLEADOS = "empleados.dat";
+const char* NOMBRE_ARCHIVO_CUENTAS = "cuentas.dat";
+const char* NOMBRE_ARCHIVO_PRESTAMOS = "prestamos.dat";
+const char* NOMBRE_ARCHIVO_TRANSACCIONES = "transacciones.dat";
 
 // DATOS FIJOS DE LA CUENTA DEL BANCO
-const char* const ID_CUENTA_BANCO = "0001";
-const char* const ID_CLIENTE_BANCO = "BANK0"; // Un ID ficticio para el "cliente" banco
-const char* const NOMBRE_CUENTA_BANCO = "Fondos Centrales del Banco";
-const char* const CVU_CUENTA_BANCO = "0000000001";
-const char* const ALIAS_CUENTA_BANCO = "WENNER.BANK.FONDOS";
+const char* ID_CUENTA_BANCO = "0001";
+const char* ID_CLIENTE_BANCO = "BANK0"; // Un ID ficticio para el "cliente" banco
+const char* NOMBRE_CUENTA_BANCO = "Fondos Centrales del Banco";
+const char* CVU_CUENTA_BANCO = "0000000001";
+const char* ALIAS_CUENTA_BANCO = "WENNER.BANK.FONDOS";
 // 
 //  CuentaBancaria cuentaDelBanco(
 //             ID_CUENTA_BANCO,
